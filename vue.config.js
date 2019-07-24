b943c8c6e2952da952f3a383a0f592624c831bdb
Rubén Asensio Fernández <ruben.asenfer@gmail.com>
2019-07-24 18:05:03 +0200

    [ADD] mockserver WIP

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,22 @@ const packageConf = require('./package.json');
 process.env.VUE_APP_VERSION = packageConf.version;
 
 module.exports = {
-    "publicPath": "./"
+    "publicPath": "./",
+    pluginOptions: {
+        "serve-api-mocks": {
+            base: "/api",
+            routes: [
+                {
+                    method: "PUT",
+                    path: "/*",
+                    callback(req, res) {
+                        res
+                            .status(200)
+                            .send("OK")
+                            .end();
+                    },
+                },
+            ],
+        },
+    }
 }
