e6f18711cb8f2911895f8bdabe05c270bf40fc9c
Rubén Asensio Fernández <ruben.asenfer@gmail.com>
2019-07-26 17:43:12 +0200

    [REF] realocate footer ouside content but amintain allow inside

--- src/views/main-view/MainView.vue
+++ src/views/main-view/MainView.vue
@@ -2,17 +2,15 @@
   <div>
     <div class="container-fluid">
       <Header class="header row" toggle="menu-view" :logo="logo" />
-      <div class="row body">
-        <div id="menu-view" class="menu col-sm-2 collapse navbar-collapse">
+      <div class="row body withFooter">
+        <div id="menu-view" class="menu withFooter col-sm-2 collapse navbar-collapse">
           <Menu :links="links" />
         </div>
-        <div class="page">
-          <div class="content withFooter">
-            <slot />
-          </div>
-          <div class="footer">v{{version}}{{env !== 'production' ? ' - env: ' + env : ''}}</div>
+        <div class="content withFooter">
+          <slot />
         </div>
       </div>
+      <div class="footer">v{{version}}{{env !== 'production' ? ' - env: ' + env : ''}}</div>
     </div>
   </div>
 </template>
