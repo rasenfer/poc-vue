d40c9d8fd89ee1afe8373b904b0b8aa41bbdbd89
Rubén Asensio Fernández <ruben.asenfer@gmail.com>
2019-07-27 11:35:53 +0200

    [UPD] api with axios interceptors

--- src/core/Api.js
+++ src/core/Api.js
@@ -1,74 +1,35 @@
 import apiUrls from "@/app/api-urls";
 import axios from 'axios';
 
-const DEFAULT_HEADERS = {
-    'Content-Type': 'application/json'
-};
+function isHandlerEnabled (config = {}) {
+    return !config.hasOwnProperty('handlerEnabled') || config.handlerEnabled;
+}
 
-const validStatuses = [
-    200, 201, 202, 203, 204,
-    300, 301, 302, 303, 304
-];
-
-function getHeaders(multipart = false) {
-    let headers = DEFAULT_HEADERS
-
-    if (multipart) {
-        headers = {}
-    }
-
-    if (localStorage && localStorage.token) {
-        headers = {
-            'Authorization': `JWT ${localStorage.token}`,
-            ...headers
+function requestHandler(request) {
+    if (isHandlerEnabled(request)) {
+        if (localStorage && localStorage.token) {
+            request.headers['Authorization'] = `Bearer ${localStorage.token}`
         }
+        request.headers['X-Requested-With'] = window.appName
     }
-    
-    headers = {
-        ...headers,
-        "X-Requested-With": window.appName
-    }
-
-    return headers
+    return request
 }
 
-export function checkResponse(response) {
-    if (validStatuses.includes(response.status)) {
-        return response
+function errorHandler(error) {
+    if (isHandlerEnabled(error.config)) {
+        // Handle errors
     }
-
-    // If not authorized then reset token
-    // and redirect to login page
-    if (localStorage && response.status === 401) {
-        localStorage.removeItem('token')
-        return Promise.reject(new Error('USER_ANONYMOUS'))
-    }
-
-    let err = new Error(response.statusText)
-    err.response = response
-
-    return Promise.reject(err)
+    return Promise.reject({ ...error })
 }
 
-export function processAPIErrors(apiErrors) {
-    let errors = {}
-
-    if (apiErrors) {
-        for (let key of Object.keys(apiErrors)) {
-            let err = apiErrors[key]
-
-            errors[key] = err
-
-            if (typeof err === Object || err.hasOwnProperty('length')) {
-                errors[key] = apiErrors[key][0]
-            }
-        }
+function successHandler (response) {
+    if (isHandlerEnabled(response.config)) {
+        // Handle responses
     }
-
-    return errors
+    return response
 }
 
-export function qs(params) {
+function queryString(params) {
     return (
         Object
             .keys(params)
@@ -77,38 +38,34 @@ export function qs(params) {
     )
 }
 
-function Api(basePath) {
-    return {
-        get: (uri, data = {}) => {
-            if (Object.keys(data).length > 0) {
-                uri = `${uri}?${qs(data)}`
-            }
-            return fetch(`${basePath}${uri}`, {
-                headers: getHeaders()
-            })
-        },
-        post: (uri, data) => {
-            return axios.post(`${basePath}${uri}`, data, {
-                headers: getHeaders()
-            })
-        },
-        put: (uri, data) => {
-            return axios.put(`${basePath}${uri}`, data, {
-                headers: getHeaders()
-            })
-        },
-        delete: (uri) => {
-            return axios.delete(`${basePath}${uri}`, {
-                headers: getHeaders()
-            })
-        },
-        upload: (uri, data) => {
-            return fetch(`${basePath}${uri}`, {
-                headers: getHeaders(true),
-                method: 'POST',
-                body: data
-            })
+class Api {
+    constructor(basePath) {
+        this.basePath = basePath;
+        this.axiosInstance = axios.create({
+            baseURL: basePath
+        });
+        this.axiosInstance.interceptors.request.use(
+            request => requestHandler(request)
+        );
+        this.axiosInstance.interceptors.response.use(
+            response => successHandler(response),
+            error => errorHandler(error)
+        );
+    }
+    get(uri, data = {},) {
+        if (Object.keys(data).length > 0) {
+            uri = `${uri}?${queryString(data)}`
         }
+        return this.axiosInstance.get(uri)
+    }
+    post(uri, data) {
+        return this.axiosInstance.post(uri, data)
+    }
+    put(uri, data) {
+        return this.axiosInstance.put(uri, data)
+    }
+    delete(uri) {
+        return this.axiosInstance.delete(uri)
     }
 }
 
