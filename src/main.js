1e13844b0a0681ecf9583b2da43a9ab102a9d1fd
Rubén Asensio Fernández <ruben.asenfer@gmail.com>
2019-07-28 13:57:36 +0200

    [REF] config core moved to core

--- src/main.js
+++ src/main.js
@@ -1,27 +1,5 @@
-import 'whatwg-fetch';
-
-import Vue from "vue";
-
-import "@/config/bootstrap-config";
-import "@/config/lodash-config";
-import router from "@/config/router-config";
-import store from "@/config/vuex-config";
-const packageInfo = require("../package.json");
+import config from "@/core/config/app-config";
 
 import App from "@/app/App";
 
-const production = process.env.NODE_ENV === 'production';
-Vue.config.devtools = !production;
-Vue.config.productionTip = production;
-
-window.appName = packageInfo.name;
-
-const app = new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
-
-if(!production && window.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
-  window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app.constructor;
-}
+config(App);
