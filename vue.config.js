c307b62156f20b084b3ae08fef943e4b7f79599b
Rubén Asensio Fernández <ruben.asenfer@gmail.com>
2019-08-08 12:41:31 +0200

    [UPD] webpack-chunk configured

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const packageConf = require('./package.json');
 const apiMock = require('./api/apiMock.js');
+const path = require('path');
+const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 process.env.VUE_APP_VERSION = packageConf.version;
-var DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 module.exports = {
   pluginOptions: {
     'serve-api-mocks': apiMock,
@@ -11,6 +12,36 @@ module.exports = {
   configureWebpack: {
     resolve: {
       plugins: [new DirectoryNamedWebpackPlugin()],
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vue: {
+            test: /[\\/]node_modules[\\/](vue|vuex|vue-types|vue-router|vue-loader)[\\/]/,
+            name: 'chunk-vue',
+          },
+          utils: {
+            test: /[\\/]node_modules[\\/](lodash|util|axios)[\\/]/,
+            name: 'chunk-utils',
+          },
+          bootstrap: {
+            test: /[\\/]node_modules[\\/](bootstrap|)[\\/]/,
+            name: 'chunk-bootstrap',
+          },
+          jquery: {
+            test: /[\\/]node_modules[\\/](jquery)[\\/]/,
+            name: 'chunk-jquery',
+          },
+          polyfill: {
+            test: /[\\/]node_modules[\\/](core-js|whatwg-fetch)[\\/]/,
+            name: 'chunk-polyfill',
+          },
+        },
+      },
     },
   },
 };
